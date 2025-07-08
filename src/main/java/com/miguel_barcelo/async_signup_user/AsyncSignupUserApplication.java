f97d0a6eb48711d6f5b0748a2a1d8352645e1cf8
Miguel Barcelo <miguel.barcelo@gmail.com>
2025-07-08 13:21:14 -0700

    feat: init project

---
+++ src/main/java/com/miguel_barcelo/async_signup_user/AsyncSignupUserApplication.java
@@ -0,0 +1,13 @@
+package com.miguel_barcelo.async_signup_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncSignupUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncSignupUserApplication.class, args);
+	}
+
+}
