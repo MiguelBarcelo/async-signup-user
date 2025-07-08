a690e9d083f08c63ee614f2e1e18c8f9450c6629
Miguel Barcelo <miguel.barcelo@gmail.com>
2025-07-08 14:33:12 -0700

    feat: add DTO

---
+++ src/main/java/com/miguel_barcelo/async_signup_user/dto/UserRequestDTO.java
@@ -0,0 +1,12 @@
+package com.miguel_barcelo.async_signup_user.dto;
+
+public class UserRequestDTO {
+	private String name;
+	private String email;
+
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
+	
+	public String getEmail() { return email; }
+	public void setEmail(String email) { this.email = email; }
+}
