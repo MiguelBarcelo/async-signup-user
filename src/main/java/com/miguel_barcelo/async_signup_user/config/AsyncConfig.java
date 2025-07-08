bd246e5d2ea672e46ae7c233b86998e9763e4f60
Miguel Barcelo <miguel.barcelo@gmail.com>
2025-07-08 14:34:04 -0700

    feat: add Configuration

---
+++ src/main/java/com/miguel_barcelo/async_signup_user/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package com.miguel_barcelo.async_signup_user.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+	@Bean(name = "taskExecutor")
+	Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(4);
+		executor.setQueueCapacity(10);
+		executor.setThreadNamePrefix("async-task-");
+		executor.initialize();
+		
+		return executor;
+	}
+}
